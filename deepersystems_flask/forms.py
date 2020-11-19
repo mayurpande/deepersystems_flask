cda7e15a38a641b2f41f2c378cda9572da861458
mayur <mayurpande.uk@gmail.com>
2020-11-19 14:47:49 +0100

    created forms for dislikes and likes, added the forms and routes

--- deepersystems_flask/forms.py
+++ deepersystems_flask/forms.py
@@ -1,10 +1,27 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, Form, FormField
-from wtforms.validators import InputRequired, ValidationError
-import re
+from wtforms import StringField, SubmitField, HiddenField
+from wtforms.validators import InputRequired
+from deepersystems_flask.extentions import mongo
 
 
 class HomePage(FlaskForm):
 
-    video_name = StringField('Video Name', validators=[InputRequired()])
-    theme_name = StringField('Theme name', validators=[InputRequired()])
+    video_name = StringField('Video Name')
+    theme_name = StringField('Theme name')
+
+
+class LikeButtonForm(FlaskForm):
+    submit = SubmitField('like')
+    hidden = HiddenField('hidden')
+
+
+class DisLikeButtonForm(FlaskForm):
+    submit = SubmitField('dislike')
+    hidden = HiddenField('hidden')
+
+
+
+
+
+
+
